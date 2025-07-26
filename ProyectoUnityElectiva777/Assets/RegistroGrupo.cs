134a3a3d872cb102371b77cd0f2ccb597d8c34e5
Danpo2 <est.jaime.posada@unimilitar.edu.co>
2025-07-25 20:25:47 -0500

    Inicio del script RegistroGrupo.cs por Daniel Posada

---
+++ ProyectoUnityElectiva777/Assets/RegistroGrupo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RegistroGrupo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //	Nombre	completo: Jaime Daniel Posada Gómez	
+        //	Fecha:	25/07/2025
+        //	Hora:	8:14pm	
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
