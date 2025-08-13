228aba5e2154592fc45396e8cf34fb85a9da2cfd
Danpo2 <est.jaime.posada@unimilitar.edu.co>
2025-08-13 15:48:38 -0500

    Escena Touch

---
+++ Guia_2/Assets/Script/Touchphase.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class Touchphase : MonoBehaviour
+{
+    public TextMeshProUGUI PhaseDisplayText;
+    private Touch theTouch;
+    private float timeTouchEnded;
+    private float displayTime = .5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            theTouch = Input.GetTouch(0);
+            PhaseDisplayText.SetText(theTouch.phase.ToString());
+
+            if (theTouch.phase == TouchPhase.Ended)
+            {
+                timeTouchEnded = Time.time;
+            }
+        }
+        else if (Time.time - timeTouchEnded > displayTime)
+        {
+            PhaseDisplayText.SetText("");
+        }
+    }
+
+}
